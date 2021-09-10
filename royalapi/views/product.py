bf5841b80be287793a8b3f9caba4262acbc2e7cc
Devin Garcia <devinmgarcia@gmail.com>
2021-09-10 09:58:17 -0500

    crud for products complete

--- royalapi/views/product.py
+++ royalapi/views/product.py
@@ -38,3 +38,42 @@ class Products(ViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         except Exception as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+  
+        try:
+            product = Product.objects.get(pk=pk)
+            serializer = ProductSerializer(product, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+    def update(self, request, pk=None):
+        # get the product
+        product = Product.objects.get(pk=pk)
+        # set the products new values
+        product.title = request.data["title"]
+        product.price = request.data["price"]
+        product.in_stock = request.data["in_stock"]
+        # get the product type object
+        product_type = ProductType.objects.get(pk=request.data['type_id'])
+        # set the product type value
+        product.type_id = product_type
+        # save changes
+        product.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+    
+        try:
+            product = Product.objects.get(pk=pk)
+            product.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Product.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
